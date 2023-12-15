51cc6d5911d37e8d3f85290e07a4f1a631b96cf2
BOOBATHI THILLAN <128199038+BOOBATHITHILLAN@users.noreply.github.com>
2023-12-15 08:38:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import roomRouter from "./routes/roomRouter.js";
 import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
-
+import cors from 'cors'
 dotenv.config();
 
 const app = express();
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   );
   next();
 });
-
+app.use(cors())
 app.use(express.json({ limit: "10mb" }));
 app.use("/user", userRouter);
 app.use("/room", roomRouter);
